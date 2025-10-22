f1c212afc721b4069cdc3651e07588e0b11b3fbc
Ivo Rocha <ivocunha077@gmail.com>
2025-10-22 17:56:32 +0100

    Dados

--- trabalho.hs
+++ trabalho.hs
@@ -1,2 +1,13 @@
 module Trabalho where
 import System.IO
+
+------- Dados
+
+type JogadorAVE = (String, Int, Int, Float)
+type TorneioAVE = [JogadorAVE]
+type ResultadoAVE = (Int, String, String, String)
+type ResultadosAVE = [ResultadoAVE]
+type Equipa = String
+type TorneioElim = [Equipa]
+type ResultadoElim = (Int, String, String, String)
+type ResultadosElim = [ResultadoElim]
